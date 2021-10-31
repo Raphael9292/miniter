472f30702df384ad2e92771a5bb2914864db3187
raphael9292 <kyeongseong.kim@kurlycorp.com>
2021-10-31 16:03:27 +0900

    s3 사용을 위한 config 주입

--- config.py
+++ config.py
@@ -13,6 +13,11 @@ DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db
 JWT_SECRET_KEY = 'SOME_SUPER_SECRET_KEY'
 JWT_EXP_DELTA_SECONDS = 7 * 24 * 60 * 60
 
+S3_BUCKET = "test-bucket"
+S3_ACCESS_KEY = "s3-access-key"
+S3_SECRET_KEY = "s3-secrete-key"
+S3_BUCKET_URL = f"https://s3.ap-northeast-2.amazonaws.com/{S3_BUCKET}/"
+
 test_db = {
     'user': 'root',
     'password': 'test1234',
